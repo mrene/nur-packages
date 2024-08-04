36d745b4ebf6d658a92cb59f1d38f66fab911e2d
Mathieu Rene <mathieu.rene@gmail.com>
2024-08-04 11:21:22 -0400

    init: add packages from dotfiles repo

---
+++ pkgs/default/gnuradioWithPackages/package.nix
@@ -0,0 +1,8 @@
+{ lib, newScope, callPackage, gnuradio, gnuradioPackages, ... }@args:
+
+(gnuradio.override {
+  extraPackages = builtins.attrValues gnuradioPackages;
+  extraPythonPackages = with gnuradio.python.pkgs; [ soapysdr ];
+} // args) // {
+  pkgs = gnuradio.pkgs.overrideScope (final: prev: gnuradioPackages);
+}
