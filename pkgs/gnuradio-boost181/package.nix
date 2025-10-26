e119f88d6a7b44d7f5813ffdad669186b07239ad
Mathieu Rene <mathieu.rene@gmail.com>
2025-10-26 09:46:17 -0400

    openthread-border-router: fix Python3 and CMake issues
    Add python3 to nativeBuildInputs and add CMake policy flag
    to work around cJSON submodule compatibility issues with
    newer CMake versions.

--- pkgs/gnuradio-boost181/package.nix
+++ pkgs/gnuradio-boost181/package.nix
@@ -3,7 +3,10 @@
 # Recent updates upgraded boost to a version which deprecated a lot of things causing a lot of breakage
 # Since boost versions have to match, override it to the last known working version
 gnuradio.override ({
-  unwrapped = gnuradio.unwrapped.override { 
+  unwrapped = gnuradio.unwrapped.overrideAttrs (old: {
     boost = boost181;
-  };
+    patches = (old.patches or []) ++ [
+      ./fix-boost-format-formatter.patch
+    ];
+  });
 } // (removeAttrs args ["boost181" "gnuradio"] ))  
