65dc238e193d2fa3b72e9793adaa50c3f595fd2f
Mathieu Rene <mathieu.rene@gmail.com>
2026-05-17 12:46:00 -0400

    clock-weather-card: 2.8.3 -> 2.9.3

--- pkgs/clock-weather-card/package.nix
+++ pkgs/clock-weather-card/package.nix
@@ -1,53 +1,97 @@
 { lib
+, stdenv
 , fetchFromGitHub
-, mkYarnPackage
-, fetchYarnDeps
+, cacert
+, nodejs
+, yarn-berry
 }:
 
-mkYarnPackage rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "clock-weather-card";
-  version = "2.8.3";
+  version = "2.9.3";
 
   src = fetchFromGitHub {
     owner = "pkissling";
     repo = "clock-weather-card";
-    rev = "v${version}";
-    hash = "sha256-Qcem4ixSzBHah8WbAwaieB3GgurWSo9y5Ty2Yvx/mGU=";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-CeOrJFdvl9O6TF9E2W34mR6VmLnTzhXGmrBmbtsBLxA=";
   };
 
-  packageJSON = ./package.json;
+  yarnOfflineCache = stdenv.mkDerivation {
+    pname = "${finalAttrs.pname}-yarn-deps";
+    inherit (finalAttrs) version src;
 
-  offlineCache = fetchYarnDeps {
-    yarnLock = src + "/yarn.lock";
-    hash = "sha256-hu1AVAfnr9K4Bv8tHE/HWSTg+ZrWEf+MldRodqv8zTU=";
+    nativeBuildInputs = [ yarn-berry ];
+
+    dontInstall = true;
+
+    env = {
+      YARN_ENABLE_TELEMETRY = 0;
+      NODE_EXTRA_CA_CERTS = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    };
+
+    configurePhase = ''
+      runHook preConfigure
+
+      export HOME="$NIX_BUILD_TOP"
+      yarn config set enableGlobalCache false
+      yarn config set cacheFolder $out
+
+      runHook postConfigure
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+
+      mkdir -p $out
+      yarn install --immutable --mode skip-build
+
+      runHook postBuild
+    '';
+
+    outputHash = "sha256-fi5qN1M31hlH9+M0LiP2aDuF7cRxHgaBG+L6aeUATHU=";
+    outputHashMode = "recursive";
   };
+
+  nativeBuildInputs = [
+    nodejs
+    yarn-berry
+  ];
+
+  configurePhase = ''
+    runHook preConfigure
+
+    export HOME="$NIX_BUILD_TOP"
+    yarn config set enableGlobalCache false
+    yarn config set cacheFolder $yarnOfflineCache
+
+    runHook postConfigure
+  '';
 
   buildPhase = ''
     runHook preBuild
 
+    yarn install --immutable --immutable-cache
     yarn build
-    
+
     runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
 
-    mkdir $out
-    cp ./deps/clock-weather-card/dist/clock-weather-card.js $out/
+    mkdir -p $out
+    cp dist/clock-weather-card.js $out/
 
     runHook postInstall
   '';
 
-  doDist = false;
-
   meta = with lib; {
     description = "A Home Assistant Card indicating today's date/time, along with an iOS inspired weather forecast for the next days with animated icons";
     homepage = "https://github.com/pkissling/clock-weather-card";
-    changelog = "https://github.com/pkissling/clock-weather-card/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/pkissling/clock-weather-card/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ mrene ];
-    mainProgram = "clock-weather-card";
     platforms = platforms.linux;
   };
-}
+})
